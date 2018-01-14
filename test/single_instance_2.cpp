c3da8661dce363b34f30541ad7ea43997d8c2fb2
Peter Dimov <pdimov@pdimov.com>
2018-01-14 05:10:44 +0200

    Add test that checks for duplicate category instances

---
+++ test/single_instance_2.cpp
@@ -0,0 +1,22 @@
+
+// Copyright 2018 Peter Dimov.
+// Distributed under the Boost Software License, Version 1.0.
+
+#include <boost/system/error_code.hpp>
+
+using namespace boost::system;
+
+namespace lib2
+{
+
+error_code get_system_code()
+{
+    return error_code( 0, system_category() );
+}
+
+error_code get_generic_code()
+{
+    return error_code( 0, generic_category() );
+}
+
+} // namespace lib2
