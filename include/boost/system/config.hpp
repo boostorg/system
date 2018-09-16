9d86d0296ef1ebddd93b09ddb046bf9481195f2b
Peter Dimov <pdimov@pdimov.com>
2018-09-16 07:04:27 +0300

    Revert boost/system/config.hpp

--- include/boost/system/config.hpp
+++ include/boost/system/config.hpp
@@ -1,23 +1,40 @@
-#ifndef BOOST_SYSTEM_CONFIG_HPP_INCLUDED
-#define BOOST_SYSTEM_CONFIG_HPP_INCLUDED
+//  boost/system/config.hpp  -----------------------------------------------------------//
 
-// Copyright Beman Dawes 2003, 2006
-// Copyright 2018 Peter Dimov
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
-// See http://www.boost.org/libs/system for documentation.
+//  Copyright Beman Dawes 2003, 2006
 
-// This header is no longer used by Boost.System.
-// Its use is discouraged and it will be deprecated.
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+//  See http://www.boost.org/libs/system for documentation.
+
+#ifndef BOOST_SYSTEM_CONFIG_HPP                  
+#define BOOST_SYSTEM_CONFIG_HPP
 
 #include <boost/config.hpp>
+#include <boost/predef/platform.h>
+#include <boost/system/api_config.hpp>  // for BOOST_POSIX_API or BOOST_WINDOWS_API
 
-// Included for compatibility. Not used.
-#include <boost/system/api_config.hpp>
+// This header implements separate compilation features as described in
+// http://www.boost.org/more/separate_compilation.html
 
-// Definition of BOOST_SYSTEM_DECL retained for compatibility.
+//  normalize macros  ------------------------------------------------------------------//
+
+#if !defined(BOOST_SYSTEM_DYN_LINK) && !defined(BOOST_SYSTEM_STATIC_LINK) \
+  && !defined(BOOST_ALL_DYN_LINK) && !defined(BOOST_ALL_STATIC_LINK)
+# define BOOST_SYSTEM_STATIC_LINK
+#endif
+
+#if defined(BOOST_ALL_DYN_LINK) && !defined(BOOST_SYSTEM_DYN_LINK)
+# define BOOST_SYSTEM_DYN_LINK 
+#elif defined(BOOST_ALL_STATIC_LINK) && !defined(BOOST_SYSTEM_STATIC_LINK)
+# define BOOST_SYSTEM_STATIC_LINK 
+#endif
+
+#if defined(BOOST_SYSTEM_DYN_LINK) && defined(BOOST_SYSTEM_STATIC_LINK)
+# error Must not define both BOOST_SYSTEM_DYN_LINK and BOOST_SYSTEM_STATIC_LINK
+#endif
+
+//  enable dynamic or static linking as requested --------------------------------------//
 
 #if defined(BOOST_ALL_DYN_LINK) || defined(BOOST_SYSTEM_DYN_LINK)
 # if defined(BOOST_SYSTEM_SOURCE)
@@ -29,4 +46,25 @@
 # define BOOST_SYSTEM_DECL
 #endif
 
-#endif // BOOST_SYSTEM_CONFIG_HPP_INCLUDED
+//  enable automatic library variant selection  ----------------------------------------// 
+
+#if !defined(BOOST_SYSTEM_SOURCE) && !defined(BOOST_ALL_NO_LIB) && !defined(BOOST_SYSTEM_NO_LIB)
+//
+// Set the name of our library, this will get undef'ed by auto_link.hpp
+// once it's done with it:
+//
+#define BOOST_LIB_NAME boost_system
+//
+// If we're importing code from a dll, then tell auto_link.hpp about it:
+//
+#if defined(BOOST_ALL_DYN_LINK) || defined(BOOST_SYSTEM_DYN_LINK)
+#  define BOOST_DYN_LINK
+#endif
+//
+// And include the header that does the work:
+//
+#include <boost/config/auto_link.hpp>
+#endif  // auto-linking disabled
+
+#endif // BOOST_SYSTEM_CONFIG_HPP
+
