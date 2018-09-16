34600a62dfe60995e8407e8c3e427fca28f215c3
Peter Dimov <pdimov@pdimov.com>
2018-09-16 06:15:22 +0300

    The constexpr workarounds are no longer needed

--- include/boost/system/detail/config.hpp
+++ include/boost/system/detail/config.hpp
@@ -17,7 +17,7 @@
 #endif
 
 // BOOST_SYSTEM_NOEXCEPT
-// Retained for backward compatibility only
+// Retained for backward compatibility
 
 #define BOOST_SYSTEM_NOEXCEPT BOOST_NOEXCEPT
 
@@ -25,22 +25,6 @@
 
 #if !defined(BOOST_NO_CXX14_CONSTEXPR)
 # define BOOST_SYSTEM_HAS_CONSTEXPR
-#endif
-
-#if defined(__GNUC__) && (__GNUC__ == 7 && __GNUC_MINOR__ < 4) && __cplusplus >= 201700L
-// https://gcc.gnu.org/bugzilla/show_bug.cgi?id=83835
-#  undef BOOST_SYSTEM_HAS_CONSTEXPR
-#endif
-
-#if defined(__clang__) && defined(_MSC_VER) && defined(_CPPLIB_VER)
-// Clang on Windows with MSVC headers, the constructor of std::error_category
-// is not constexpr at least up to VS2017 15.7.x (_MSVC_STL_UPDATE 201803)
-#  undef BOOST_SYSTEM_HAS_CONSTEXPR
-#endif
-
-#if defined(__clang__) && defined(BOOST_LIBSTDCXX_VERSION) && BOOST_LIBSTDCXX_VERSION < 40900
-// The constructor of std::error_category is not constexpr in libstdc++ 4.8
-#  undef BOOST_SYSTEM_HAS_CONSTEXPR
 #endif
 
 #if defined(BOOST_SYSTEM_HAS_CONSTEXPR)
