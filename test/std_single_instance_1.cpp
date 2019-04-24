907c867cd10ff6a6ee2028a959f71092e6836f7f
Peter Dimov <pdimov@gmail.com>
2019-04-24 21:15:39 +0300

    Rename test/std_ec_mismatch to std_single_instance; test also static and shared libraries

--- test/std_single_instance_1.cpp
+++ test/std_single_instance_1.cpp
@@ -2,25 +2,33 @@
 // Copyright 2019 Peter Dimov.
 // Distributed under the Boost Software License, Version 1.0.
 
+#include <boost/config.hpp>
+
+#if defined(STD_SINGLE_INSTANCE_DYN_LINK)
+# define EXPORT BOOST_SYMBOL_EXPORT
+#else
+# define EXPORT
+#endif
+
 #include <boost/system/error_code.hpp>
 
 #if defined(BOOST_SYSTEM_HAS_SYSTEM_ERROR)
 
 #include <system_error>
 
-namespace lib2
+namespace lib1
 {
 
-std::error_code get_system_code()
+EXPORT std::error_code get_system_code()
 {
     return boost::system::error_code( 0, boost::system::system_category() );
 }
 
-std::error_code get_generic_code()
+EXPORT std::error_code get_generic_code()
 {
     return boost::system::error_code( 0, boost::system::generic_category() );
 }
 
-} // namespace lib2
+} // namespace lib1
 
 #endif
