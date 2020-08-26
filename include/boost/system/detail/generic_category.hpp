c345b86b0a60348b3fe90ba68db06958a67ec70f
Peter Dimov <pdimov@gmail.com>
2020-08-27 02:56:18 +0300

    Restore generic_category_impl.hpp; functions are needed by system_category

--- include/boost/system/detail/generic_category.hpp
+++ include/boost/system/detail/generic_category.hpp
@@ -11,6 +11,7 @@
 //  See library home page at http://www.boost.org/libs/system
 
 #include <boost/system/detail/error_category.hpp>
+#include <boost/system/detail/generic_category_impl.hpp>
 #include <boost/system/detail/config.hpp>
 #include <boost/cstdint.hpp>
 #include <boost/config.hpp>
@@ -56,81 +57,15 @@ public:
 
 // generic_error_category::message
 
-#if defined(__GLIBC__)
-
-// glibc has two incompatible strerror_r definitions
-
-inline char const * strerror_r_helper( char const * r, char const * ) BOOST_NOEXCEPT
-{
-    return r;
-}
-
-inline char const * strerror_r_helper( int r, char const * buffer ) BOOST_NOEXCEPT
-{
-    return r == 0? buffer: "Unknown error";
-}
-
 inline char const * generic_error_category::message( int ev, char * buffer, std::size_t len ) const BOOST_NOEXCEPT
 {
-    return strerror_r_helper( strerror_r( ev, buffer, len ), buffer );
+    return generic_error_category_message( ev, buffer, len );
 }
 
 inline std::string generic_error_category::message( int ev ) const
 {
-    char buffer[ 128 ];
-    return generic_error_category_message( ev, buffer, sizeof( buffer ) );
+    return generic_error_category_message( ev );
 }
-
-#else // #if defined(__GLIBC__)
-
-// std::strerror is thread-safe on everything else, incl. Windows
-
-# if defined( BOOST_MSVC )
-#  pragma warning( push )
-#  pragma warning( disable: 4996 )
-# elif defined(__clang__) && defined(__has_warning)
-#  pragma clang diagnostic push
-#  if __has_warning("-Wdeprecated-declarations")
-#   pragma clang diagnostic ignored "-Wdeprecated-declarations"
-#  endif
-# endif
-
-inline std::string generic_error_category::message( int ev ) const
-{
-    char const * m = std::strerror( ev );
-    return m? m: "Unknown error";
-}
-
-inline char const * generic_error_category::message( int ev, char * buffer, std::size_t len ) const BOOST_NOEXCEPT
-{
-    if( len == 0 )
-    {
-        return buffer;
-    }
-
-    if( len == 1 )
-    {
-        buffer[0] = 0;
-        return buffer;
-    }
-
-    char const * m = std::strerror( ev );
-
-    if( m == 0 ) return "Unknown error";
-
-    std::strncpy( buffer, m, len - 1 );
-    buffer[ len-1 ] = 0;
-
-    return buffer;
-}
-
-# if defined( BOOST_MSVC )
-#  pragma warning( pop )
-# elif defined(__clang__) && defined(__has_warning)
-#  pragma clang diagnostic pop
-# endif
-
-#endif // #if defined(__GLIBC__)
 
 } // namespace detail
 
