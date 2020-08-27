a93572ab160fe5cbafe183d07496c1711630d487
Peter Dimov <pdimov@gmail.com>
2020-08-27 03:23:00 +0300

    Extract error_category implementation into its own header

--- include/boost/system/error_code.hpp
+++ include/boost/system/error_code.hpp
@@ -20,6 +20,7 @@
 #include <boost/system/detail/error_condition.hpp>
 #include <boost/system/detail/error_code.hpp>
 #include <boost/system/detail/throws.hpp>
+#include <boost/system/detail/error_category_impl.hpp>
 #include <boost/system/api_config.hpp>
 #include <boost/system/detail/config.hpp>
 #include <boost/cstdint.hpp>
@@ -92,71 +93,6 @@ BOOST_SYSTEM_CONSTEXPR inline error_condition make_error_condition( errc_t e ) B
 }
 
 } // namespace errc
-
-// error_category default implementation
-
-inline error_condition error_category::default_error_condition( int ev ) const BOOST_NOEXCEPT
-{
-    return error_condition( ev, *this );
-}
-
-inline bool error_category::equivalent( int code, const error_condition & condition ) const BOOST_NOEXCEPT
-{
-    return default_error_condition( code ) == condition;
-}
-
-inline bool error_category::equivalent( const error_code & code, int condition ) const BOOST_NOEXCEPT
-{
-    return *this == code.category() && code.value() == condition;
-}
-
-inline char const * error_category::message( int ev, char * buffer, std::size_t len ) const BOOST_NOEXCEPT
-{
-    if( len == 0 )
-    {
-        return buffer;
-    }
-
-    if( len == 1 )
-    {
-        buffer[0] = 0;
-        return buffer;
-    }
-
-#if !defined(BOOST_NO_EXCEPTIONS)
-    try
-#endif
-    {
-        std::string m = this->message( ev );
-
-# if defined( BOOST_MSVC )
-#  pragma warning( push )
-#  pragma warning( disable: 4996 )
-# elif defined(__clang__) && defined(__has_warning)
-#  pragma clang diagnostic push
-#  if __has_warning("-Wdeprecated-declarations")
-#   pragma clang diagnostic ignored "-Wdeprecated-declarations"
-#  endif
-# endif
-
-        std::strncpy( buffer, m.c_str(), len - 1 );
-        buffer[ len-1 ] = 0;
-
-# if defined( BOOST_MSVC )
-#  pragma warning( pop )
-# elif defined(__clang__) && defined(__has_warning)
-#  pragma clang diagnostic pop
-# endif
-
-        return buffer;
-    }
-#if !defined(BOOST_NO_EXCEPTIONS)
-    catch( ... )
-    {
-        return "Message text unavailable";
-    }
-#endif
-}
 
 } // namespace system
 
