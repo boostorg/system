98533dedb320d0bfe9be5a0ba0f1876318ea8e1a
Peter Dimov <pdimov@gmail.com>
2021-06-15 05:32:04 +0300

    Disable failing tests on g++ 4.8 and 4.9

--- test/std_interop_test7.cpp
+++ test/std_interop_test7.cpp
@@ -5,6 +5,7 @@
 #include <boost/system/error_code.hpp>
 #include <boost/core/lightweight_test.hpp>
 #include <boost/config/pragma_message.hpp>
+#include <boost/config.hpp>
 #include <cerrno>
 
 #if !defined(BOOST_SYSTEM_HAS_SYSTEM_ERROR)
@@ -34,8 +35,15 @@ int main()
         BOOST_TEST( e1 == boost::system::errc::bad_address );
         BOOST_TEST_NOT( e1 != boost::system::errc::bad_address );
 
+#if defined(BOOST_GCC) && BOOST_GCC >= 40800 && BOOST_GCC < 50000
+
+// fails on g++ 4.8.5 and g++ 4.9.4 from ubuntu-toolchain-r-test for unknown reasons
+// works on the system g++ 4.8.4 on Trusty and the system g++ 4.8.5 on CentOS 7
+
+#else
         BOOST_TEST( e1 == std::errc::bad_address );
         BOOST_TEST_NOT( e1 != std::errc::bad_address );
+#endif
     }
 
     return boost::report_errors();
