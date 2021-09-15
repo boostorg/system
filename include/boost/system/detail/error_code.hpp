cd98f4edd784ac5e2eeb98bbcf9d98d7d5a00330
Peter Dimov <pdimov@gmail.com>
2021-09-15 07:42:57 +0300

    Update system_error to incorporate the source location in what()

--- include/boost/system/detail/error_code.hpp
+++ include/boost/system/detail/error_code.hpp
@@ -3,7 +3,7 @@
 
 //  Copyright Beman Dawes 2006, 2007
 //  Copyright Christoper Kohlhoff 2007
-//  Copyright Peter Dimov 2017, 2018
+//  Copyright Peter Dimov 2017-2021
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
