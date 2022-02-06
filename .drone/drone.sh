a5c1ab042e4d75ee69e26753792bafac567e5a8b
Peter Dimov <pdimov@gmail.com>
2022-02-06 22:25:45 +0200

    Update Drone files

--- .drone/drone.sh
+++ .drone/drone.sh
@@ -13,8 +13,6 @@ export GIT_COMMIT=$DRONE_COMMIT
 export REPO_NAME=$DRONE_REPO
 export PATH=~/.local/bin:/usr/local/bin:$PATH
 
-if [ "$DRONE_JOB_BUILDTYPE" == "boost" ]; then
-
 echo '==================================> INSTALL'
 
 BOOST_BRANCH=develop && [ "$TRAVIS_BRANCH" == "master" ] && BOOST_BRANCH=master || true
@@ -31,88 +29,3 @@ echo '==================================> SCRIPT'
 
 echo "using $TOOLSET : : $COMPILER ;" > ~/user-config.jam
 ./b2 -j3 libs/system/test toolset=$TOOLSET cxxstd=$CXXSTD variant=debug,release ${ADDRMD:+address-model=$ADDRMD} ${UBSAN:+cxxflags=-fsanitize=undefined cxxflags=-fno-sanitize-recover=undefined linkflags=-fsanitize=undefined define=UBSAN=1 debug-symbols=on} ${LINKFLAGS:+linkflags=$LINKFLAGS}
-
-elif [ "$DRONE_JOB_BUILDTYPE" == "aa8f93b806-b342e8ae9b" ]; then
-
-echo '==================================> INSTALL'
-
-BOOST_BRANCH=develop && [ "$TRAVIS_BRANCH" == "master" ] && BOOST_BRANCH=master || true
-cd ..
-git clone -b $BOOST_BRANCH --depth 1 https://github.com/boostorg/boost.git boost-root
-cd boost-root
-git submodule update --init tools/boostdep
-cp -r $TRAVIS_BUILD_DIR/* libs/system
-python tools/boostdep/depinst/depinst.py system
-./bootstrap.sh
-./b2 headers
-
-echo '==================================> SCRIPT'
-
-mkdir __build__ && cd __build__
-cmake -DBOOST_ENABLE_CMAKE=1 -DBoost_VERBOSE=1 -DBOOST_INCLUDE_LIBRARIES=system -DBUILD_TESTING=ON ..
-ctest --output-on-failure -R boost_system
-
-elif [ "$DRONE_JOB_BUILDTYPE" == "aa8f93b806-0b4c708508" ]; then
-
-echo '==================================> INSTALL'
-
-BOOST_BRANCH=develop && [ "$TRAVIS_BRANCH" == "master" ] && BOOST_BRANCH=master || true
-cd ..
-git clone -b $BOOST_BRANCH --depth 1 https://github.com/boostorg/boost.git boost-root
-cd boost-root
-git submodule update --init tools/boostdep
-cp -r $TRAVIS_BUILD_DIR/* libs/system
-python tools/boostdep/depinst/depinst.py system
-./bootstrap.sh
-./b2 headers
-
-echo '==================================> SCRIPT'
-
-mkdir __build__ && cd __build__
-cmake -DBOOST_ENABLE_CMAKE=1 -DBoost_VERBOSE=1 -DBUILD_SHARED_LIBS=ON -DBOOST_INCLUDE_LIBRARIES=system -DBUILD_TESTING=ON ..
-ctest --output-on-failure -R boost_system
-
-elif [ "$DRONE_JOB_BUILDTYPE" == "5e4a4d0f23-fda7c76df5" ]; then
-
-echo '==================================> INSTALL'
-
-BOOST_BRANCH=develop && [ "$TRAVIS_BRANCH" == "master" ] && BOOST_BRANCH=master || true
-git clone -b $BOOST_BRANCH https://github.com/boostorg/assert.git ../assert
-git clone -b $BOOST_BRANCH https://github.com/boostorg/config.git ../config
-git clone -b $BOOST_BRANCH https://github.com/boostorg/core.git ../core
-git clone -b $BOOST_BRANCH https://github.com/boostorg/predef.git ../predef
-git clone -b $BOOST_BRANCH https://github.com/boostorg/winapi.git ../winapi
-
-echo '==================================> SCRIPT'
-
-cd test/cmake_subdir_test && mkdir __build__ && cd __build__
-cmake ..
-cmake --build .
-cmake --build . --target check
-
-elif [ "$DRONE_JOB_BUILDTYPE" == "aa8f93b806-e569545176" ]; then
-
-echo '==================================> INSTALL'
-
-BOOST_BRANCH=develop && [ "$TRAVIS_BRANCH" == "master" ] && BOOST_BRANCH=master || true
-cd ..
-git clone -b $BOOST_BRANCH --depth 1 https://github.com/boostorg/boost.git boost-root
-cd boost-root
-git submodule update --init tools/boostdep
-cp -r $TRAVIS_BUILD_DIR/* libs/system
-python tools/boostdep/depinst/depinst.py system
-./bootstrap.sh
-./b2 headers
-
-echo '==================================> SCRIPT'
-
-pip install --user cmake
-mkdir __build__ && cd __build__
-cmake -DBOOST_ENABLE_CMAKE=1 -DBoost_VERBOSE=1 -DBOOST_INCLUDE_LIBRARIES="system;core" -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=~/.local ..
-cmake --build . --target install
-cd ../libs/system/test/cmake_install_test && mkdir __build__ && cd __build__
-cmake -DCMAKE_INSTALL_PREFIX=~/.local ..
-cmake --build .
-cmake --build . --target check
-
-fi
