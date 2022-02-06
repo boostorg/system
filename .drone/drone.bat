34dcb59ee80b329dc40c6a3f84bf5c2de18494d0
Peter Dimov <pdimov@gmail.com>
2022-02-06 23:30:19 +0200

    Update drone.bat

--- .drone/drone.bat
+++ .drone/drone.bat
@@ -1,22 +1,20 @@
 @ECHO ON
 
-set TRAVIS_OS_NAME=windows
+set DRONE_BUILD_DIR=%CD%
 
-IF "!DRONE_BRANCH!" == "" (
-  for /F %%i in ("!GITHUB_REF!") do @set TRAVIS_BRANCH=%%~nxi
-) else (
-  SET TRAVIS_BRANCH=!DRONE_BRANCH!
+IF "%DRONE_BRANCH%" == "" (
+  for /F %%i in ("%GITHUB_REF%") do @set DRONE_BRANCH=%%~nxi
 )
 
 for /F %%i in ("%DRONE_REPO%") do @set SELF=%%~nxi
 
 set BOOST_BRANCH=develop
-if "%TRAVIS_BRANCH%" == "master" set BOOST_BRANCH=master
+if "%DRONE_BRANCH%" == "master" set BOOST_BRANCH=master
 cd ..
 git clone -b %BOOST_BRANCH% --depth 1 https://github.com/boostorg/boost.git boost-root
 cd boost-root
 git submodule update --init tools/boostdep
-xcopy /s /e /q %APPVEYOR_BUILD_FOLDER% libs\%SELF%\
+xcopy /s /e /q %DRONE_BUILD_DIR% libs\%SELF%\
 python tools/boostdep/depinst/depinst.py %SELF%
 cmd /c bootstrap
 b2 -d0 headers
