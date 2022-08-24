a2df4d09da27a5c3f579bf8800024faa71b2d3ba
Peter Dimov <pdimov@gmail.com>
2022-08-24 21:41:38 +0300

    Update error_code_test3.cpp

--- test/error_code_test3.cpp
+++ test/error_code_test3.cpp
@@ -53,11 +53,17 @@ int main()
     BOOST_TEST_EQ( ec, en );
     BOOST_TEST_EQ( en, ec );
 
+    BOOST_TEST_EQ( ec, enomem_c );
+    BOOST_TEST_EQ( enomem_c, ec );
+
     BOOST_TEST_EQ( ec, enomem_n );
     BOOST_TEST_EQ( enomem_n, ec );
 
     BOOST_TEST_EQ( en, enomem_c );
     BOOST_TEST_EQ( enomem_c, en );
+
+    BOOST_TEST_EQ( en, enomem_n );
+    BOOST_TEST_EQ( enomem_n, en );
 
     return boost::report_errors();
 }
