f332a52597fcf6e2adea856aa9a367ee1070f948
Peter Dimov <pdimov@gmail.com>
2023-01-22 04:54:46 +0200

    Disable C4800 under msvc-14.0

--- test/result_value_construct5.cpp
+++ test/result_value_construct5.cpp
@@ -2,6 +2,10 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
+#if defined(_MSC_VER) && _MSC_VER < 1910
+# pragma warning( disable: 4800 ) // forcing value to bool 'true' or 'false'
+#endif
+
 #include <boost/system/result.hpp>
 #include <boost/core/lightweight_test.hpp>
 #include <boost/core/lightweight_test_trait.hpp>
