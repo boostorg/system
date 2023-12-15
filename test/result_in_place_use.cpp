18edbf75d080c1eaaca476d875ae9573e15568b1
Peter Dimov <pdimov@gmail.com>
2023-12-15 02:44:14 +0200

    Test ::in_place_* for result specializations. Refs #115.

--- test/result_in_place_use.cpp
+++ test/result_in_place_use.cpp
@@ -2,7 +2,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // https://www.boost.org/LICENSE_1_0.txt
 
-#include <boost/system.hpp>
+#include <boost/system/result.hpp>
 #include <boost/core/lightweight_test.hpp>
 
 template<class T, class U> void test( T const& t, U const& u )
@@ -15,6 +15,8 @@ int main()
     using namespace boost::system;
 
     test( result<int>::in_place_value, result<int>::in_place_error );
+    test( result<void>::in_place_value, result<void>::in_place_error );
+    test( result<int&>::in_place_value, result<int&>::in_place_error );
 
     return boost::report_errors();
 }
