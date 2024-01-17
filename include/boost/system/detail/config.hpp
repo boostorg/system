945b0591dd0ac65cb44fe98dfd39cba8f455b14f
Peter Dimov <pdimov@gmail.com>
2024-01-17 04:14:23 +0200

    Unconditionally enable BOOST_SYSTEM_HAS_SYSTEM_ERROR

--- include/boost/system/detail/config.hpp
+++ include/boost/system/detail/config.hpp
@@ -12,17 +12,15 @@
 #include <boost/config.hpp>
 #include <boost/config/workaround.hpp>
 
-// BOOST_SYSTEM_HAS_SYSTEM_ERROR
-//
 // The macro BOOST_SYSTEM_DISABLE_THREADS can be defined on configurations
 // that provide <system_error> and <atomic>, but not <mutex>, such as the
 // single-threaded libstdc++.
 //
 // https://github.com/boostorg/system/issues/92
 
-#if !defined(BOOST_NO_CXX11_HDR_SYSTEM_ERROR) && !defined(BOOST_NO_CXX11_HDR_ATOMIC) && ( !defined(BOOST_NO_CXX11_HDR_MUTEX) || defined(BOOST_SYSTEM_DISABLE_THREADS) )
-# define BOOST_SYSTEM_HAS_SYSTEM_ERROR
-#endif
+// BOOST_SYSTEM_HAS_SYSTEM_ERROR
+
+#define BOOST_SYSTEM_HAS_SYSTEM_ERROR
 
 // BOOST_SYSTEM_NOEXCEPT
 // Retained for backward compatibility
